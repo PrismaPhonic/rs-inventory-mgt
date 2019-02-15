33b171ee692cb4d4d790eea50cfc732470bb6802
Peter Farr <Peter@PrismaPhonic.com>
2019-02-14 23:18:24 -0800

    Added documentation comments, a readme, and filled out Cargo.toml in anticipation of publishing the crate

---
+++ README.md
@@ -0,0 +1,56 @@
+## Inventory Management
+[![Build Status](https://travis-ci.org/PrismaPhonic/rs-inventory-mgt.svg?branch=master)](https://travis-ci.org/PrismaPhonic/rs-inventory-mgt)
+[![crates.io](http://meritbadge.herokuapp.com/inventory-mgt)](https://crates.io/crates/inventory-mgt)
+[![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
+[![Released API docs](https://docs.rs/birdseed/inventory-mgt.svg)](https://docs.rs/inventory-mgt)
+
+This crate is a port of an existing python project. It allows one to sync the current
+quantities from a supply inventory csv file to a filtered down view they have created of their
+own truncated "master inventory" (their own inventory)
+
+For examples of master inventory and supply inventory csv files, look in the
+root directory of this repo.
+
+### This program will update the quantity in master inventory with the quantity found in supply inventory based for each part in master inventory
+
+
+- The default filenames that program accepts are:
+   - "SupplyInventory.csv" for the _supply list_
+   - "MasterInventory.csv" for the _master list_
+- _You can set your own filenames but you must specify their name and location in command line arguments_
+   - _See how to use command line arguments below_
+
+
+- Both csv files must include the following columns:
+
+   | VenCode | PartNumber | TotalQty |
+   | ------- |:----------:|:--------:|
+
+### Installation
+
+You can install this application in one of two ways.  Either clone the repo and build the
+release version with cargo, or simply use cargo install:
+
+```terminal
+$ cargo install inventory-mgt
+```
+
+### Use
+
+To use this application, simply run it with the generate command to generate a new master csv
+with updated quantity fields:
+
+```terminal
+$ inventory-mgt generate
+```
+
+Make sure you are in the root folder where your `SupplyInventory.csv` and `MasterInventory.csv`
+files are. You can grab sample ones from the github repo.
+
+Optionally you can pass in custom filenames with flags after the `generate` command:
+
+```terminal
+$ inventory-mgt generate -m masterinv.csv -s supplyinv.csv
+```
+
+That's it! Enjoy!
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,53 @@
+//! # Inventory Management
+//! 
+//! This crate is a port of an existing python project. It allows one to sync the current
+//! quantities from a supply inventory csv file to a filtered down view they have created of their
+//! own truncated "master inventory" (their own inventory)
+//! 
+//! ### This program will update the quantity in master inventory with the quantity found in supply inventory based for each part in master inventory
+//! 
+//! 
+//! - The default filenames that program accepts are:  
+//!    - "SupplyInventory.csv" for the _supply list_  
+//!    - "MasterInventory.csv" for the _master list_   
+//! - _You can set your own filenames but you must specify their name and location in command line arguments_
+//!    - _See how to use command line arguments below_
+//! 
+//! 
+//! - Both csv files must include the following columns:  
+//! 
+//!    | VenCode | PartNumber | TotalQty |  
+//!    | ------- |:----------:|:--------:|  
+//! 
+//! ## Installation
+//!
+//! You can install this application in one of two ways.  Either clone the repo and build the
+//! release version with cargo, or simply use cargo install:
+//!
+//! ```terminal
+//! $ cargo install inventory-mgt
+//! ```
+//! 
+//! ## Use
+//!
+//! To use this application, simply run it with the generate command to generate a new master csv
+//! with updated quantity fields:
+//!
+//! ```terminal
+//! $ inventory-mgt generate
+//! ```
+//!
+//! Make sure you are in the root folder where your `SupplyInventory.csv` and `MasterInventory.csv`
+//! files are. You can grab sample ones from the github repo.
+//!
+//! Optionally you can pass in custom filenames with flags after the `generate` command:
+//!
+//! ```terminal
+//! $ inventory-mgt generate -m masterinv.csv -s supplyinv.csv
+//! ```
+//!
+//! That's it! Enjoy!
+
 #[macro_use]
 extern crate structopt;
 
@@ -41,6 +91,7 @@ pub enum InventoryConfig {
  * CSV Structs
  */
 
+/// Provides the structures for a part from the Master csv list
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct MasterPart {
@@ -156,12 +207,4 @@ pub fn run(config: InventoryConfig) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
